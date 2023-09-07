9973b81eee1a65d2b02752dd1f1d135f03745e56
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-07 15:30:18 -0400

    Time: 35 ms (85.11%) | Memory: 13.4 MB (19.69%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reverse_num = 0
+        while x > reverse_num:
+            reverse_num = reverse_num * 10 + x % 10
+            x //= 10
+        return x == reverse_num or x == reverse_num // 10
