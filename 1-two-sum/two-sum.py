df53b49a07d09fefa5e846bd99953f8f812dfdc5
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-07 11:50:57 -0400

    Time: 36 ms (87.33%) | Memory: 14.3 MB (32.55%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,25 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        Index = {}
-        for i in range(len(nums)):
-            if target - nums[i] in Index:
-                return [Index[target - nums[i]], i]
-            Index[nums[i]] = i
-        return [] 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #Create a dictionary to store the values and their indices
+        num_dict = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            # Check if the complement exists in the dictionary
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            
+            # If not, add the current number and its index to 
+            #the   dictionary
+            num_dict[num] = i
+        
+        # If no solution is found, return an empty list or 
+        #raise an exception
+        return []        
         
