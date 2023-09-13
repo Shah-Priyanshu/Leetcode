308e88366b623426cf2c2a8adf63b25f702ff8d8
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-13 14:49:05 -0400

    Time: 15 ms (75.31%) | Memory: 13.2 MB (86.22%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -6,8 +6,12 @@ class Solution(object):
         :rtype: int
         """
         k = 0  # Initialize the count of non-val elements
-        for i in range(len(nums)):
+        n = len(nums)
+        
+        for i in range(n):
             if nums[i] != val:
-                nums[k] = nums[i]  # Move non-val elements to the front of the array
+                if i != k:  # Avoid unnecessary assignments when the value is not equal to val
+                    nums[k], nums[i] = nums[i], nums[k]  # Swap the elements
                 k += 1  # Increment the count of non-val elements
+                
         return k
