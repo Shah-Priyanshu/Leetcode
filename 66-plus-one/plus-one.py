5c31b9c7d045a5af4e5d6ea27afb67f51dfbfb84
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-14 13:45:49 -0400

    Time: 12 ms (82.46%) | Memory: 13.1 MB (98.26%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,18 +4,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        # Initialize carry to 1 to add 1 to the least significant digit
-        carry = 1
-        
-        # Iterate through the digits in reverse order
-        for i in range(len(digits) - 1, -1, -1):
-            current_sum = digits[i] + carry
-            # Update the current digit and reset carry
-            digits[i] = current_sum % 10
-            carry = current_sum // 10
-    
-        # If there's a carry left after the loop, add it as a new most significant digit
-        if carry > 0:
-            digits.insert(0, carry)
-    
-        return digits
+        res=0
+        for i in digits:
+            res=res*10+i
+        res=res+1
+        return [int(digit) for digit in str(res)]
