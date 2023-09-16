b4ccb810c17b1edfb5c65d955f32064d5504415f
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-16 16:41:24 -0400

    Time: 19 ms (63.24%) | Memory: 13.2 MB (49.05%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,27 +4,13 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-         # Handle special cases for 0 and 1
-        if x == 0 or x == 1:
-            return x
+        if x == 0:
+            return 0
 
-        # Binary search for the square root
-        left, right = 0, x
-        while left <= right:
-            mid = (left + right) // 2
-            mid_squared = mid * mid
-
-            # If the middle value squared is equal to x, return mid
-            if mid_squared == x:
-                return mid
-
-            # If mid_squared is less than x, search the right half
-            elif mid_squared < x:
-                left = mid + 1
-
-            # If mid_squared is greater than x, search the left half
-            else:
-                right = mid - 1
-
-        # When the binary search ends, return the integer part of the last mid
-        return right
+        # Initialize with an initial guess
+        guess = x
+        while True:
+            new_guess = (guess + x // guess) // 2
+            if new_guess >= guess:
+                return int(guess)
+            guess = new_guess
