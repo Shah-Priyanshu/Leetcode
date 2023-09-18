e0ea673451a0498b129494fa6cb3b7f4aaf99660
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-18 10:05:35 -0400

    Time: 20 ms (54.11%) | Memory: 13.1 MB (97.85%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,19 +7,6 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        # Initialize pointers for nums1 and nums2 and a pointer for the merged array
-        p1, p2, p = m - 1, n - 1, m + n - 1
-        
-        # While there are elements in both arrays
-        while p1 >= 0 and p2 >= 0:
-            # Compare the elements from the end of both arrays
-            if nums1[p1] >= nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            p -= 1
-        
-        # If there are remaining elements in nums2, copy them to nums1
-        nums1[:p2 + 1] = nums2[:p2 + 1]
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        nums1.sort()
