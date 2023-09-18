8b0e556a9d84b61d837b84caf08a1e41f605cb79
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-17 23:36:04 -0400

    Time: 20 ms (14.15%) | Memory: 13.1 MB (81.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<=2:
-            return n
-        a,b = 1,2
-        for i in range(3,n+1):
-            a,b = b,a+b
-        return b
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        # Create an array to store the number of ways to reach each step
+        dp = [0] * (n + 1)
+        dp[1] = 1  # There is 1 way to reach the 1st step
+        dp[2] = 2  # There are 2 ways to reach the 2nd step
+        
+        for i in range(3, n + 1):
+            # The number of ways to reach the current step is the sum of
+            # the number of ways to reach the previous two steps
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
