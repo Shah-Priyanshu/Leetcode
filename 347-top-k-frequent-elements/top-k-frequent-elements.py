7d57432376e40dc298ba6ef2943eae809c580401
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-23 13:20:57 -0400

    Time: 62 ms (97.75%) | Memory: 16.6 MB (80.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,28 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # Create a Counter to count the frequency of elements in nums
-        frequency_counter = Counter(nums)
-        
-        # Create a min-heap to store the k most frequent elements
-        min_heap = []
-
-        # Iterate through the unique elements in frequency_counter
-        for num, freq in frequency_counter.items():
-            # Push elements into the heap along with their frequencies
-            # Negative frequency is used because heapq is a min-heap
-            heapq.heappush(min_heap, (freq, num))
-
-            # If the heap size exceeds k, pop the smallest element
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-        
-        # Extract the k most frequent elements from the heap
-        result = []
-        while min_heap:
-            result.append(min_heap.pop()[1])
-        
-        # Reverse the result list to get the elements in the correct order
-        result.reverse()
-
-        return result
+        d = {}
+        for item in nums:
+            if item in d:
+                d[item] += 1
+            else:
+                d[item] = 1
+        sorted_dict = sorted(d.items(), key=lambda x:x[1], reverse=True)
+        l = []
+        for key in sorted_dict:
+            if k > 0:
+                l.append(key[0])
+                k -= 1
+        return l
