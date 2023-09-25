cc554edd11769f716745b61177c3c59844e979ad
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-24 23:16:14 -0400

    Time: 7 ms (99.00%) | Memory: 13.1 MB (95.22%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -4,32 +4,23 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        # Define the maximum and minimum 32-bit integer values
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
-
-        # Initialize the result
-        result = 0
-
-        # Handle negative numbers
+        final = 0
+            
+        negative = False
         if x < 0:
-            sign = -1
+            negative = True
             x = abs(x)
+
+        stringX = str(x)[::-1]
+        backwards = int(stringX)
+        #digits = [int(i) for i in stringX]
+
+        if negative == True:
+            final = backwards*-1
         else:
-            sign = 1
+            final = backwards
 
-        while x != 0:
-            # Extract the last digit of x
-            digit = x % 10
-
-            # Check for integer overflow
-            if result > (INT_MAX - digit) // 10:
-                return 0
-
-            # Add the digit to the result
-            result = result * 10 + digit
-
-            # Remove the last digit from x
-            x //= 10
-
-        return result * sign
+        if final > (2**31 - 1) or final < (-1*(2**31)):
+            final = 0
+        
+        return final
