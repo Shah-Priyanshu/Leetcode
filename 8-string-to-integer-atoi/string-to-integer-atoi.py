71c27710a7c36f6921f7b5115c8db182ff648d47
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-25 21:20:32 -0400

    Time: 7 ms (99.31%) | Memory: 13.3 MB (58.16%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -4,40 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Remove leading whitespace
-        s = s.lstrip()
-
-        # Check if the string is empty after removing whitespace
-        if not s:
+        s = s.strip()
+        multiplier = 1
+        if len(s) == 0:
             return 0
-
-        # Check for the sign (positive or negative)
-        sign = 1
-        if s[0] == '-':
-            sign = -1
+        if s[0] == '+':
             s = s[1:]
-        elif s[0] == '+':
+        elif s[0] == '-':
+            multiplier = -1
             s = s[1:]
-
-        # Initialize the result
-        result = 0
-
-        # Iterate through the characters and convert to integer
-        for char in s:
-            if char.isdigit():
-                result = result * 10 + int(char)
+        arr = deque()
+        for i in s:
+            if i.isdigit():
+                arr.appendleft(int(i))
             else:
                 break
-
-        # Apply the sign
-        result *= sign
-
-        # Clamp the result to the 32-bit signed integer range
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
-        if result > INT_MAX:
-            return INT_MAX
-        elif result < INT_MIN:
-            return INT_MIN
+        place = 1
+        ret = 0
+        while(len(arr) > 0):
+            ret = ret + place * arr.popleft()
+            place = place * 10
+        if ret >= 2147483648:
+            if multiplier == 1:
+                return 2147483647
+            else:
+                return -2147483648
         else:
-            return result
+            return ret * multiplier
