919cbb9d1b69d36a232a77d50744f8559f02e85e
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-27 19:44:11 -0400

    Time: 27 ms (73.54%) | Memory: 13.1 MB (94.94%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -4,23 +4,50 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        val = [
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4, 1
-        ]
-        syb = [
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV",
-            "I"
-        ]
+        dup = num
+        ans = ""
         
-        roman_num = ""
-        
-        for i in range(len(val)):
-            count = num // val[i]
-            roman_num += syb[i] * count
-            num -= val[i] * count
-        
-        return roman_num
+        if dup >= 1000:
+            temp = dup//1000
+            dup -= temp * 1000
+            ans += "M"*temp
+        if dup >= 900:
+            dup -= 900
+            ans += "CM"
+        if dup >= 500:
+            dup -= 500
+            ans += "D"
+        if dup >= 400 :
+            dup -= 400
+            ans += "CD"
+        if dup >= 100:
+            temp = dup//100
+            dup -= temp * 100
+            ans += "C"*temp
+        if dup >= 90:
+            dup -= 90
+            ans += "XC"
+        if dup >= 50 :
+            dup -= 50
+            ans += "L"
+        if dup >= 40 :
+            dup -= 40
+            ans += "XL"
+        if dup >= 10:
+            temp = dup//10
+            dup -= temp * 10
+            ans += "X"*temp
+        if dup >= 9:
+            dup -= 9
+            ans += "IX"
+        if dup >= 5:
+            dup -= 5
+            ans += "V"
+        if dup >= 4:
+            dup -= 4
+            ans += "IV"
+        if dup >= 1:
+            temp = dup//1
+            dup -= temp * 1
+            ans += "I"*temp
+        return ans
