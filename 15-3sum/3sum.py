f5c7ed64f63b614d1723184f3d18f242bdf96f38
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-28 18:01:07 -0400

    Time: 214 ms (99.96%) | Memory: 17.3 MB (15.37%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,29 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()  # Sort the input array in ascending order.
-        triplets = []
-        
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicates to avoid duplicate triplets.
-            
-            left, right = i + 1, len(nums) - 1  # Initialize two pointers.
-            
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                
-                if total == 0:
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1  # Skip duplicates on the left.
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1  # Skip duplicates on the right.
-                    left += 1
-                    right -= 1
-                elif total < 0:
-                    left += 1
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        nums = sorted(count)
+        result = []
+        for i, num in enumerate(nums):
+            if count[num] > 1:
+                if num == 0:
+                    if count[num] > 2:
+                        result.append([0, 0, 0])
                 else:
-                    right -= 1
-        
-        return triplets
+                    if -2 * num in count:
+                        result.append([num, num, -2 * num])
+            if num < 0:
+                two_sum = -num
+                left = bisect.bisect_left(nums, (two_sum - nums[-1]), i + 1)
+                for i in nums[left:bisect.bisect_right(nums, (two_sum // 2), left)]:
+                    j = two_sum - i
+                    if j in count and j != i:
+                        result.append([num, i, j])
+                        
+        return result
