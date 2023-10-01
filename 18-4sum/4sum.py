41fe02d7d7f002248407f7776f22c1f938ce1ad5
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-09-30 22:55:50 -0400

    Time: 31 ms (100.00%) | Memory: 18.2 MB (7.61%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -5,29 +5,57 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        nums.sort()  # Sort the array in ascending order
-        quadruplets = []
+        if (len(nums) < 4):
+            return []
         
-        for i in range(len(nums) - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicates for i
-            for j in range(i + 1, len(nums) - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue  # Skip duplicates for j
-                left, right = j + 1, len(nums) - 1  # Initialize two pointers
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-                    if total == target:
-                        quadruplets.append([nums[i], nums[j], nums[left], nums[right]])
-                        # Skip duplicates for left and right
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
-                        left += 1
-                        right -= 1
-                    elif total < target:
-                        left += 1
-                    else:
-                        right -= 1
-        return quadruplets
+        nums.sort()
+        min_sum = nums[0]+nums[1]+nums[2]+nums[3]
+        max_sum = nums[-1]+nums[-2]+nums[-3]+nums[-4]
+
+        if target < min_sum or target > max_sum:
+            return []
+        
+        res = []
+        num_map = {}
+        num_list = []
+        sum_map = {}
+        for n in nums:
+            if n not in num_map:
+                num_map[n] = 1
+                num_list.append(n)
+            else:
+                num_map[n] += 1
+        
+        for i in range(len(num_list)):
+            for j in range(i, len(num_list)):
+                n = num_list[i]
+                m = num_list[j]
+                if n != m or num_map[n] >= 2:
+                    s = m + n
+                    if s not in sum_map:
+                        sum_map[s] = []
+                    sum_map[s].append((n, m))
+        
+        for k in sum_map.keys():
+            d = target - k
+
+            if d in sum_map and k < d:
+                for x, y in sum_map[k]:
+                    for m, l in sum_map[d]:
+                        if x == y and y == m and m < l and num_map[x] >= 3:
+                            res.append([x, x, x, l])
+                        elif x < y and y == m and m == l and num_map[l] >= 3:
+                            res.append([x, l, l, l])
+                        elif x < y and y == m and m < l and num_map[y] >= 2:
+                            res.append([x, y, y, l])
+                        elif y < m:
+                            res.append([x, y, m, l])
+
+        if target % 4 == 0:
+            n = target // 4 
+            if n in num_map and num_map[n] >= 4:
+                res.append([n, n, n, n])
+
+        
+
+        return sorted(list(res))
