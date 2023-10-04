2e4e4874a523405ec6e38090930d7ed7a3e603d8
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-10-04 19:16:04 -0400

    Time: 14 ms (65.03%) | Memory: 13.2 MB (90.25%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -9,22 +9,8 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        dummy = ListNode(-1)
-        dummy.next = head
-        prev_node = dummy
-        
-        while head and head.next:
-            # Nodes to be swapped
-            first_node = head
-            second_node = head.next
-            
-            # Swapping logic
-            prev_node.next = second_node
-            first_node.next = second_node.next
-            second_node.next = first_node
-            
-            # Reinitializing the head and prev_node for next pair
-            prev_node = first_node
-            head = first_node.next
-            
-        return dummy.next
+        if not head or not head.next:
+            return head
+
+        head.next.next,head.next,head=head,self.swapPairs(head.next.next),head.next 
+        return head
