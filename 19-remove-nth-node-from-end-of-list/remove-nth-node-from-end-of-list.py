af7e062550f8642e56a07970ff126d5c9fd474a9
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-10-03 22:17:21 -0400

    Time: 15 ms (70.17%) | Memory: 13.2 MB (47.81%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -10,24 +10,13 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        # Create a dummy node to handle edge cases
-        dummy = ListNode(0)
-        dummy.next = head
-
-        # Initialize two pointers, first and second
-        first = dummy
-        second = dummy
-
-        # Move the first pointer n+1 steps ahead
-        for _ in range(n + 1):
-            first = first.next
-
-        # Move both pointers until the first pointer reaches the end
-        while first is not None:
-            first = first.next
-            second = second.next
-
-        # Remove the nth node by updating the next pointer of the previous node
-        second.next = second.next.next
-
-        return dummy.next
+        fast, slow = head, head
+        while n > 0: #advance fast n pointers
+            fast = fast.next
+            n -= 1
+        if not fast:
+            return head.next
+        while fast and fast.next is not None:
+            slow, fast = slow.next, fast.next
+        slow.next = slow.next.next
+        return head
