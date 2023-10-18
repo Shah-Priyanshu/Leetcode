6449f53ba8028676f17a0409c4115271c079055d
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-10-18 14:55:12 -0400

    Time: 14 ms (97.73%) | Memory: 13.2 MB (69.87%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,17 +1,18 @@
+def hello(n):
+        if(n==1):
+            return '1'
+        else:
+            res=hello(n-1)+'@'
+            aux=""
+            c=1
+            for i in range(len(res)-1):
+                    if(res[i]==res[i+1]):
+                           c+=1
+                    else:
+                        aux+=str(c)+res[i]
+                        c=1
+                         
+            return aux
 class Solution(object):
     def countAndSay(self, n):
-        if n == 1:
-            return "1"
-        
-        prev = self.countAndSay(n - 1)
-        result = ""
-        count = 1
-        
-        for i in range(len(prev)):
-            if i < len(prev) - 1 and prev[i] == prev[i + 1]:
-                count += 1
-            else:
-                result += str(count) + prev[i]
-                count = 1
-        
-        return result
+        return hello(n)
