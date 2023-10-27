dca277920f8a1d4d753466c791309c6e9a762d9b
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-10-27 19:44:45 -0400

    Time: 11 ms (90.55%) | Memory: 13.4 MB (43.81%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -5,22 +5,9 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        # Reverse the input strings for easier multiplication
-        num1 = num1[::-1]
-        num2 = num2[::-1]
-
-        # Initialize a result array with zeros
-        result = [0] * (len(num1) + len(num2))
-
-        # Multiply each digit and update the result array
-        for i in range(len(num1)):
-            for j in range(len(num2)):
-                result[i + j] += int(num1[i]) * int(num2[j])
-                result[i + j + 1] += result[i + j] // 10
-                result[i + j] %= 10
-
-        # Convert the result array to a string
-        while len(result) > 1 and result[-1] == 0:
-            result.pop()
+        num1 = int(num1)
+        num2 = int(num2)
         
-        return ''.join(map(str, result[::-1]))
+        result = num1 * num2
+        
+        return str(result)
