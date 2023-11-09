8571c94f3a3fcfbfc7287522673724f1e5fc20b0
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-11-08 20:19:28 -0500

    Time: 69 ms (68.83%) | Memory: 17 MB (87.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,23 +1,12 @@
-from collections import defaultdict
-
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        anagrams = defaultdict(list)
+        mp = defaultdict(list)
+        for str in strs:
+            sortedStr = "".join(sorted(str))
+            mp[sortedStr].append(str)
+        return list(mp.values())
         
-        for word in strs:
-            # Count the occurrences of each character in the word
-            count = [0] * 26
-            for char in word:
-                count[ord(char) - ord('a')] += 1
-            
-            # Convert the count list to a tuple to use it as a dictionary key
-            key = tuple(count)
-            
-            # Check if the key is already in the dictionary
-            anagrams[key].append(word)
-        
-        return list(anagrams.values())
