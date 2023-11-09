4b9bc6f18d999bab5a223634b471e4178c92d498
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-11-09 12:39:00 -0500

    Time: 13 ms (67.72%) | Memory: 13.2 MB (67.72%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -5,13 +5,4 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if n == 0:
-            return 1.0
-        if n < 0:
-            x = 1 / x
-            n = -n
-        half_pow = self.myPow(x, n // 2)
-        if n % 2 == 0:
-            return half_pow * half_pow
-        else:
-            return half_pow * half_pow * x
+        return x**n
