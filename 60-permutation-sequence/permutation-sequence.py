8b387a12a87b95555da74a900563217efbf807ff
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-11-26 19:11:43 -0500

    Time: 17 ms (51.19%) | Memory: 13.3 MB (53.17%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -5,29 +5,12 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        # Factorial function to calculate n!
-        def factorial(num):
-            result = 1
-            for i in range(1, num + 1):
-                result *= i
-            return result
-
-        # List to store digits available for permutation
-        digits = [str(i) for i in range(1, n + 1)]
-
-        # Initialize result string
-        result = ''
-
-        # Decrement k to match list indexing
+        numbers = list(range(1, n+1))
+        permutation = ''
         k -= 1
-
-        # Iterate through each digit in the range [1, n]
-        for i in range(1, n + 1):
-            # Calculate the index of the current digit in the available digits
-            index = k // factorial(n - i)
-            # Update k for the next iteration
-            k %= factorial(n - i)
-            # Append the selected digit to the result
-            result += digits.pop(index)
-
-        return result
+        while n > 0:
+            n -= 1
+            index, k = divmod(k, math.factorial(n))
+            permutation += str(numbers[index])
+            numbers.remove(numbers[index])
+        return permutation
