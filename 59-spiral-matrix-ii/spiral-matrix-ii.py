67be186f55bf9f18996a3dc24565ad136acf93ed
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-11-26 19:04:42 -0500

    Time: 18 ms (31.30%) | Memory: 13.4 MB (6.94%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -4,34 +4,36 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
-        matrix = [[0] * n for _ in range(n)]
-        num = 1
+        
+        out = [[-1] * n for i in range(n)]
 
-        left, right, top, bottom = 0, n - 1, 0, n - 1
+        row, col = None, None
+        row_dir, col_dir = 0, 1
 
-        while num <= n * n:
-            # Traverse from left to right
-            for i in range(left, right + 1):
-                matrix[top][i] = num
-                num += 1
-            top += 1
+        for i in range(1, 1 + n * n):
+            if row is None:
+                row, col = 0, 0
+            
+            else:
+                if row_dir > 0:
+                    if (row + row_dir) >= n or out[row + row_dir][col] > 0:
+                        row_dir, col_dir = 0, -1
 
-            # Traverse from top to bottom
-            for i in range(top, bottom + 1):
-                matrix[i][right] = num
-                num += 1
-            right -= 1
+                elif row_dir < 0:
+                    if (row + row_dir) < 0 or out[row + row_dir][col] > 0:
+                        row_dir, col_dir = 0, 1
 
-            # Traverse from right to left
-            for i in range(right, left - 1, -1):
-                matrix[bottom][i] = num
-                num += 1
-            bottom -= 1
+                elif col_dir > 0:
+                    if (col + col_dir) >= n or out[row][col + col_dir] > 0:
+                        row_dir, col_dir = 1, 0
+                
+                else:
+                    if (col + col_dir) < 0 or out[row][col + col_dir] > 0:
+                        row_dir, col_dir = -1, 0       
 
-            # Traverse from bottom to top
-            for i in range(bottom, top - 1, -1):
-                matrix[i][left] = num
-                num += 1
-            left += 1
+                row, col = row + row_dir, col + col_dir
+            
 
-        return matrix
+            out[row][col] = i
+
+        return out
