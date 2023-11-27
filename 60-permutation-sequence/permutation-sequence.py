a2e33aa950a26c691bbc345c68fb5e4088451000
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-11-26 19:12:10 -0500

    Time: 17 ms (51.19%) | Memory: 13.3 MB (53.17%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -5,12 +5,25 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        numbers = list(range(1, n+1))
-        permutation = ''
-        k -= 1
-        while n > 0:
-            n -= 1
-            index, k = divmod(k, math.factorial(n))
-            permutation += str(numbers[index])
-            numbers.remove(numbers[index])
-        return permutation
+        numbers = list(range(1, n + 1))
+        permutation = []
+        k -= 1  # Convert k to zero-based index
+
+        # Calculate factorial of n-1
+        factorial = 1
+        for i in range(1, n):
+            factorial *= i
+
+        # Determine each digit
+        for i in range(n - 1, 0, -1):
+            index = k // factorial
+            k %= factorial
+            permutation.append(numbers.pop(index))
+
+            factorial //= i
+
+        # Add the last number
+        permutation.append(numbers[0])
+
+        # Convert list of numbers to a string
+        return ''.join(map(str, permutation))
