923dae574cd9c0dd868a4a0c50e343fdeb337c7d
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-12-05 13:44:37 -0500

    Time: 40 ms (99.78%) | Memory: 13.8 MB (95.06%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,32 +1,38 @@
 class Solution(object):
+    from collections import deque
     def minDistance(self, word1, word2):
         """
         :type word1: str
         :type word2: str
         :rtype: int
         """
-        m, n = len(word1), len(word2)
+        l1 = len(word1)
+        l2 = len(word2)
 
-        # Ensure m is the smaller of the two lengths
-        if m > n:
-            word1, word2, m, n = word2, word1, n, m
+        if word1 == word2:
+            return 0
+        elif not word1 or not word2:
+            return max(l1, l2)
 
-        # Initialize a 1D array to store the minimum operations for each column
-        dp = [i for i in range(n + 1)]
+        num = 0
+        visited = set()
+        q = deque()
+        q.append((0, 0))
 
-        # Populate the DP array
-        for i in range(1, m + 1):
-            prev = dp[0]  # Store the value of the previous column
-            dp[0] = i     # Update the first column
+        while q:
+            for _ in range(len(q)):
+                i, j = q.popleft()
 
-            for j in range(1, n + 1):
-                temp = dp[j]
-                if word1[i - 1] == word2[j - 1]:
-                    dp[j] = prev
-                else:
-                    dp[j] = 1 + min(dp[j], dp[j - 1], prev)
-
-                prev = temp
-
-        # The result is stored in the last cell of the DP array
-        return dp[n]
+                if (i, j) in visited:
+                    continue
+                
+                visited.add((i, j))
+                while i < l1 and j < l2 and word1[i] == word2[j]:
+                    i += 1
+                    j += 1
+                if i == l1 and j == l2:
+                    return num
+                q.append((i + 1, j))
+                q.append((i, j + 1))
+                q.append((i + 1, j + 1))
+            num += 1
