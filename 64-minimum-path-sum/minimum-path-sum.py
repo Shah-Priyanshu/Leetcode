44e2f4dba042369404b8860627e8439930379cd2
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-12-04 21:49:54 -0500

    Time: 58 ms (90.38%) | Memory: 14.6 MB (60.11%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -4,26 +4,16 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        if not grid:
-            return 0
+        m = len(grid)
+        n = len(grid[0])
 
-        m, n = len(grid), len(grid[0])
-
-        # Initialize a 2D array to store minimum path sums
-        dp = [[0] * n for _ in range(m)]
-
-        # Fill the first row and first column of dp array
-        dp[0][0] = grid[0][0]
-
-        for i in range(1, m):
-            dp[i][0] = dp[i - 1][0] + grid[i][0]
-
-        for j in range(1, n):
-            dp[0][j] = dp[0][j - 1] + grid[0][j]
-
-        # Fill the rest of the dp array
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-
-        return dp[m - 1][n - 1]
+        for i in range(m):
+            for j in range(n):
+                if i > 0 and j > 0:
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+                if i > 0 and j == 0:
+                    grid[i][0] += grid[i-1][0]
+                if j > 0 and i == 0:
+                    grid[0][j] += grid[0][j-1]
+        return grid[m-1][n-1]
+        
