3d1c9f6cfa2d028aa42e21c131e822065aaec119
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2023-12-07 15:53:25 -0500

    Time: 22 ms (78.45%) | Memory: 13.8 MB (8.08%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
@@ -7,10 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        if not matrix or not matrix[0]:
-            return False
-
-        flat_matrix = np.array(matrix).flatten()
-        index = np.searchsorted(flat_matrix, target)
-
-        return index < len(flat_matrix) and flat_matrix[index] == target
+        top = 0               
+        bottom = len(matrix) - 1
+        while top <= bottom:
+            mid = (top + bottom) // 2
+            if target < matrix[mid][0]:
+                bottom = mid - 1
+            elif target > matrix[mid][-1]:
+                top = mid + 1
+            else:
+                break
+        row = mid
+        lo = 0
+        hi = len(matrix[0]) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if target < matrix[row][mid]:
+                hi = mid - 1
+            elif target > matrix[row][mid]:
+                lo = mid + 1
+            else:
+                return True
+        
+        return False
