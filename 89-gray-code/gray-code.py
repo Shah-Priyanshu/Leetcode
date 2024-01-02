10820a1a4b276cdb44101feb2ba183e1b069b02b
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-01-02 09:20:19 -0500

    Time: 149 ms (14.07%) | Memory: 19.2 MB (86.67%) - LeetSync

--- 89-gray-code/gray-code.py
+++ 89-gray-code/gray-code.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        if n == 0:
+        if n <1 or n > 16:
             return [0]
-        # Generate the (n-1)-bit Gray code sequence recursively
         prev_gray_code = self.grayCode(n - 1)
-        # Add 1 to the front of each number in the previous list in reverse order
-        return prev_gray_code + [x + (1 << (n - 1)) for x in reversed(prev_gray_code)]
+        size = 1<<(n - 1)
+        current_gray_code = prev_gray_code + [size + i for i in reversed(prev_gray_code)]
+        return current_gray_code
