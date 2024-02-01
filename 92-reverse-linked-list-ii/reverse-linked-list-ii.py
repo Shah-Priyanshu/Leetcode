b4c9554ee4523578b28afaec8d0a4e3583361312
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-02-01 13:41:46 -0500

    Time: 21 ms (20.57%) | Memory: 11.8 MB (91.33%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -0,0 +1,30 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: ListNode
+        :type left: int
+        :type right: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        # Move prev to the node before the start of the reversal
+        for _ in range(left - 1):
+            prev = prev.next
+
+        # Start the reversal process
+        current = prev.next
+        for _ in range(right - left):
+            temp = current.next
+            current.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
