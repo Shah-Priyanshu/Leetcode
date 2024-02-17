24402d1bbc903a24bddf23f9096ef053c63c7ba4
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-02-16 23:31:18 -0500

    Time: 15 ms (75.19%) | Memory: 12.3 MB (64.30%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -1,35 +1,23 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def levelOrderBottom(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        if not root:
-            return []
-
-        queue = [root]
-        results = []
-
-        while queue:
-            level_size = len(queue)
-            level_nodes = []
-
-            for _ in range(level_size):
-                node = queue.pop(0)
-                level_nodes.append(node.val)
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-            results.append(level_nodes)
-
-        return results[::-1]  # Reverse the results for bottom-up order
+        layers = []
+        def helper(root, layer, layers):
+            if not root:
+                return
+            if layer + 1 > len(layers):
+                layers.append([])
+            layers[layer].append(root.val)
+            helper(root.left, layer + 1, layers)
+            helper(root.right, layer + 1, layers)
+        helper(root, 0, layers)
+        return layers[::-1]
