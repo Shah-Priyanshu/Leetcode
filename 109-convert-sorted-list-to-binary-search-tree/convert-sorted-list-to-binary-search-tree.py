60443e88a4d8204ea28ff1dbe8ca057a400133b4
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-02-19 21:30:00 -0500

    Time: 115 ms (14.83%) | Memory: 23.9 MB (44.07%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.py
@@ -1,37 +1,26 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution:
+class Solution(object):
     def sortedListToBST(self, head):
-        # Function to convert linked list to array
-        def mapListToArray(head):
-            arr = []
-            while head:
-                arr.append(head.val)
-                head = head.next
-            return arr
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[TreeNode]
+        """
+
+        if not head:
+            return
+        inorder = []
+        while head:
+            inorder.append(head.val)
+            head = head.next
+
+        return self.BST(inorder)
+
         
-        # Function to convert sorted array to BST
-        def sortedArrayToBST(arr, start, end):
-            if start > end:
-                return None
-
-            mid = (start + end) // 2
-            node = TreeNode(arr[mid])
-            node.left = sortedArrayToBST(arr, start, mid - 1)
-            node.right = sortedArrayToBST(arr, mid + 1, end)
-            return node
-
-        # Convert the linked list to an array
-        arr = mapListToArray(head)
-
-        # Build the BST from the array
-        return sortedArrayToBST(arr, 0, len(arr) - 1)
+    def BST(self, inorder):
+        if not inorder:
+            return
+        idx = len(inorder) // 2
+        node = TreeNode(inorder[idx])
+        node.left = self.BST(inorder[:idx])
+        node.right = self.BST(inorder[idx+1:])
+        
+        return node
