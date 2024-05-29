7e37b5b2705511d7974612a1a1f5731531adfc18
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-05-29 06:26:21 -0400

    Time: 83 ms (44.60%) | Memory: 26.9 MB (99.47%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -3,14 +3,17 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+from typing import Optional
 
 class Solution:
-    def getIntersectionNode(self, headA, headB):
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         if not headA or not headB:
             return None
-
-        ptrA, ptrB = headA, headB
-        while ptrA is not ptrB:
-            ptrA = ptrA.next if ptrA else headB
-            ptrB = ptrB.next if ptrB else headA
-        return ptrA
+        
+        pointerA, pointerB = headA, headB
+        
+        while pointerA is not pointerB:
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+        
+        return pointerA
