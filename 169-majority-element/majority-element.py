7e54f8228f665164ab282a36d394ba4641b77eb5
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-06-02 17:50:25 -0400

    Time: 165 ms (80.50%) | Memory: 18 MB (73.21%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        # Phase 1: Find a candidate
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        # Phase 2: Verify the candidate
+        count = 0
+        for num in nums:
+            if num == candidate:
+                count += 1
+
+        if count > len(nums) // 2:
+            return candidate
+        else:
+            return None
+    
