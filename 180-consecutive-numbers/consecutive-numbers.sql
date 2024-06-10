df8f84a7928815209bba885beb54e0db7e62e4eb
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-06-09 20:47:31 -0400

    Time: 555 ms (95.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,13 @@
+#Approach 3-Using EXISTS and SUBQUERY 
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+WHERE EXISTS (
+    SELECT 1
+    FROM Logs l2
+    WHERE l2.id = l1.id + 1 AND l2.num = l1.num
+    AND EXISTS (
+        SELECT 1
+        FROM Logs l3
+        WHERE l3.id = l1.id + 2 AND l3.num = l1.num
+    )
+);
