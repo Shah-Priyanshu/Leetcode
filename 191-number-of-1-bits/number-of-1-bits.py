39e95c5d4e5100bac0750149a7a4bf11aa14f4a9
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-06-17 21:22:03 -0400

    Time: 39 ms (31.84%) | Memory: 16.5 MB (33.07%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,8 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        binary_representation = bin(n)[2:]
-        count_of_ones = 0
-        for bit in binary_representation:
-            if bit == '1':
-                count_of_ones += 1
-        return count_of_ones
+        result = 0
+        while n:
+            n &= n - 1  # Remove the lowest set bit
+            result += 1    # Increment the count of set bits
+        return result
