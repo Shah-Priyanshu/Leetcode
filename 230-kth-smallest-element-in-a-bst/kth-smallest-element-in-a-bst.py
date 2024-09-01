2f8f785b603ed6fb73e72ab4786d21eca72a4e1a
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-09-01 01:36:11 -0400

    Time: 50 ms (24.82%) | Memory: 19.5 MB (18.15%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,32 +1,31 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # Initialize an empty stack to simulate in-order traversal
+        stack = []
+        current = root
 
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        self.count = 0
-        self.result = None
+        # Perform iterative in-order traversal
+        while current or stack:
+            # Go to the leftmost node
+            while current:
+                stack.append(current)
+                current = current.left
+            
+            # Current is None, process the node on top of the stack
+            current = stack.pop()
+            k -= 1
+            
+            # If k is 0, we found the kth smallest element
+            if k == 0:
+                return current.val
+            
+            # Go to the right subtree
+            current = current.right
         
-        def inorder(node):
-            if not node or self.result is not None:
-                return
-            
-            inorder(node.left)
-            
-            self.count += 1
-            if self.count == k:
-                self.result = node.val
-                return
-            
-            inorder(node.right)
-        
-        inorder(root)
-        return self.result
+        return -1  # In case of an invalid input
