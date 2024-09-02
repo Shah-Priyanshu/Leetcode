29155a32e6f93909757a3ad4ba74e87593852e57
Priyanshu Shah <105989704+Shah-Priyanshu@users.noreply.github.com>
2024-09-02 16:53:10 -0400

    Time: 280 ms (82.07%) | Memory: 32.1 MB (75.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,38 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         if not head or not head.next:
-            return True
+            return True  # A list with 0 or 1 node is always a palindrome
         
-        # Find the middle of the linked list
-        slow = fast = head
-        while fast.next and fast.next.next:
+        # Step 1: Find the middle of the linked list
+        slow, fast = head, head
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        # Reverse the second half of the linked list
-        second_half = self.reverseList(slow.next)
-        
-        # Compare the first half with the reversed second half
-        first_half = head
-        while second_half:
-            if first_half.val != second_half.val:
-                return False
-            first_half = first_half.next
-            second_half = second_half.next
-        
-        return True
-    
-    def reverseList(self, head: ListNode) -> ListNode:
+
+        # Step 2: Reverse the second half of the linked list
         prev = None
-        current = head
-        while current:
-            next_temp = current.next
-            current.next = prev
-            prev = current
-            current = next_temp
-        return prev
+        while slow:
+            next_node = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_node
+
+        # Step 3: Compare the two halves
+        left, right = head, prev
+        while right:  # Only need to compare till the end of the reversed half
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        # If all elements matched, it's a palindrome
+        return True
